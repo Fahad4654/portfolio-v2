557b65190c2c8180a75a19acade3f623cca42c65
Fahad Kabir <kabirkaife@gmail.com>
2026-01-07 21:03:20 +0000

    move TypeScript JavaScript 1st

--- src/components/portfolio/SkillsSection.tsx
+++ src/components/portfolio/SkillsSection.tsx
@@ -8,7 +8,7 @@ import {
 const skillGroups = [
     {
       title: "Languages & Scripting",
-      skills: ["Java", "Python", "TypeScript", "JavaScript", "Bash scripting"],
+      skills: ["TypeScript", "JavaScript", "Java", "Python", "Bash scripting"],
     },
     {
       title: "Backend Development",
